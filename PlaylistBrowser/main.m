691c118c99fc06e30099e25f9d58710c7bad344c
lfaler <laynefaler@gmail.com>
2016-05-16 13:17:45 -0700

    Initial Commit

---
+++ PlaylistBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlaylistBrowser
+//
+//  Created by Layne Faler on 5/16/16.
+//  Copyright © 2016 Layne Faler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
